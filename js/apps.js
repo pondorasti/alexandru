40f60644d39802adab4af35c800d9b38fbc8f65c
Pondorasti <alexandru_turcanu@ymail.com>
2020-04-30 12:17:58 +0300

    Added new app

--- js/apps.js
+++ js/apps.js
@@ -1,6 +1,11 @@
 // array of apps as json
 
 var data = [{
+	"link" 		: "https://itunes.apple.com/us/app/ftc-scorer-skystone/id1493387167",
+	"img" 		: "apps/ftcscorer.png",
+	"name" 		: "FTC Scorer",
+	"details" : "Skystone"
+}, {
 	"link" 		: "https://itunes.apple.com/us/app/envision-habits-tracker/id1423771095",
 	"img" 		: "apps/envision.png",
 	"name" 		: "Envision",
