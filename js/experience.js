e6d8a318559f36690855e6b7d586fefcb3e47395
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2020-05-20 11:55:06 +0300

    Update Experience Tab

--- js/experience.js
+++ js/experience.js
@@ -1,13 +1,23 @@
 // array of experiences as json
 
 var data = [{
+	"link" 		: "https://qrobotics.eu",
+	"img" 		: "experience/QuantumRobotics.png",
+	"name" 		: "Quantum Robotics",
+	"details" : "Founder & Mentor"
+},{
+	"link" 		: "https://www.linkedin.com/in/pondorasti/",
+	"img" 		: "experience/IllumeHealth.png",
+	"name" 		: "Remote iOS Engineer",
+	"details" : "Sauna Tracker App"
+},{
 	"link" 		: "https://www.makeschool.com",
-	"img" 		: "experience/makeschool.png",
-	"name" 		: "Summer Academy 2018",
-	"details" : "iOS Developer"
+	"img" 		: "experience/MakeSchool.png",
+	"name" 		: "Summer Academy",
+	"details" : "iOS Development"
 }, {
 	"link" 		: "https://www.ichb.ro",
-	"img" 		: "experience/lumina.png",
+	"img" 		: "experience/ICHB.png",
 	"name" 		: "ICHB",
 	"details" : "Middle School & High School [2012-2020]"
 }];
