f158e128693ef2884b0077705f66d597b273d025
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2020-05-20 12:31:35 +0300

    Fixed some names - "blame git"

--- js/awards.js
+++ js/awards.js
@@ -2,7 +2,7 @@
 
 var data = [{
 	"link" 		: "https://developer.apple.com/wwdc/",
-	"img" 		: "awards/WWDC2018.png",
+	"img" 		: "awards/wwdc2018.png",
 	"name" 		: "WWDC 2018",
 	"details" : "Scholarship Winner"
 }, {
@@ -17,17 +17,17 @@ var data = [{
 	"details" : "Overall 2nd Place - Global Nominee"
 }, {
 	"link" 		: "https://github.com/Pondorasti/Pozativity",
-	"img" 		: "awards/MegaHack.png",
+	"img" 		: "awards/megahack.png",
 	"name" 		: "Techfest - Megahack 2018",
 	"details" : "Overall 2nd Place"
 }, {
 	"link" 		: "https://github.com/Pondorasti/MakeMusic",
-	"img" 		: "awards/OutsideHacks.png",
+	"img" 		: "awards/outsidehacks.png",
 	"name" 		: "OutsideHacks 2018",
 	"details" : "Finalist - Universal Magic Group Winner"
 }, {
 	"link" 		: "https://devpost.com/software/kifusf-jvl3ut",
-	"img" 		: "awards/GirlsInTech.png",
+	"img" 		: "awards/girlsInTech.png",
 	"name" 		: "Hacking for humanity 2018",
 	"details" : "Overall 3rd Place"
 }];
