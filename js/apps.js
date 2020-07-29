8b8ca1c1b85d6e891741b198517d463dfc567f31
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2020-07-29 23:15:49 +0300

    Updated: App Links

--- js/apps.js
+++ js/apps.js
@@ -1,18 +1,13 @@
 // array of apps as json
 
 var data = [{
-	"link" 		: "https://itunes.apple.com/us/app/ftc-scorer-skystone/id1493387167",
+	"link" 		: "https://github.com/Pondorasti/StonkScorer",
 	"img" 		: "apps/ftcscorer.png",
 	"name" 		: "FTC Scorer",
 	"details" : "Skystone"
 }, {
-	"link" 		: "https://itunes.apple.com/us/app/envision-habits-tracker/id1423771095",
+	"link" 		: "https://www.producthunt.com/posts/envision-2",
 	"img" 		: "apps/envision.png",
 	"name" 		: "Envision",
 	"details" : "Habits Tracker"
-}, {
-	"link" 		: "https://itunes.apple.com/us/app/kifusf/id1447746447",
-	"img" 		: "apps/kifusf.png",
-	"name" 		: "KifuSF",
-	"details" : "Donating Made Easy"
 }];
