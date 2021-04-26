bea7c8775ca9b70ccdb5fb343fb3725c5d130f7f
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-04-25 21:01:42 -0700

    Add BoringStocks

--- js/apps.js
+++ js/apps.js
@@ -1,6 +1,11 @@
 // array of apps as json
 
 var data = [{
+	"link" 		: "https://boringstocks.live",
+	"img" 		: "apps/boringstocks.png",
+	"name" 		: "BoringStocks",
+	"details" : "Stocks for the average trader"
+}, {
 	"link" 		: "https://github.com/Pondorasti/StonkScorer",
 	"img" 		: "apps/ftcscorer.png",
 	"name" 		: "FTC Scorer",
