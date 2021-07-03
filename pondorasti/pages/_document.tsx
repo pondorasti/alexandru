e0b3e6a859768dea672c777764fb29c7aaed9f4e
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-07-03 11:01:19 -0700

    Add custom document

---
+++ pondorasti/pages/_document.tsx
@@ -0,0 +1,29 @@
+import NextDocument, { Html, Head, Main, NextScript } from "next/document"
+
+export default class Document extends NextDocument {
+  render(): JSX.Element {
+    return (
+      <Html lang="en" className="nightwind">
+        <Head>
+          {/* Favicon */}
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link rel="shortcut icon" href="/favicons/favicon.ico" />
+
+          {/* Inter Font */}
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body className="max-w-7xl mx-auto bg-gray-50 dark:bg-darkGray-600">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
