1844fc778defaeb2c45456a0284fa598a6280366
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-07-03 17:56:55 -0700

    Add 90s-portfolio

--- pondorasti/next.config.js
+++ pondorasti/next.config.js
@@ -4,6 +4,10 @@ module.exports = {
       {
         source: "/secret/:github-contributions",
         destination: "/secret/:github-contributions/index.html",
+      },
+      {
+        source: "/secret/:90s-portfolio",
+        destination: "/secret/:90s-portfolio/index.html",
       }
     ]
 }
