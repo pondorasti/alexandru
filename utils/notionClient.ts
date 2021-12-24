1abee10f14874d88f7c73bf163edbfdc5d848b67
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-12-24 11:22:40 -0800

    remove: notion client

--- utils/notionClient.ts
+++
@@ -1,7 +0,0 @@
-import { Client } from "@notionhq/client"
-
-const notionToken = process.env.NEXT_PUBLIC_NOTION_TOKEN ?? ""
-
-const notion = new Client({ auth: notionToken })
-
-export default notion
