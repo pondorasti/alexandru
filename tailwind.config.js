54daa1ae6aae67412d6963c3595ddd1778c56852
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-12-24 11:23:15 -0800

    style: secrets legend

--- tailwind.config.js
+++ tailwind.config.js
@@ -43,6 +43,25 @@ module.exports = {
       colors: {
         gray: colors.slate,
       },
+      keyframes: {
+        "scale-in": {
+          "0%": { opacity: 0, transform: "scale(0)" },
+          "100%": { opacity: 1, transform: "scale(1)" },
+        },
+        "slide-down": {
+          "0%": { opacity: 0, transform: "translateY(-10px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+        "slide-up": {
+          "0%": { opacity: 0, transform: "translateY(10px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        "scale-in": "scale-in 0.2s ease-in-out",
+        "slide-down": "slide-down 0.6s cubic-bezier(0.16, 1, 0.3, 1)",
+        "slide-up": "slide-up 0.6s cubic-bezier(0.16, 1, 0.3, 1)",
+      },
     },
   },
   plugins: [
@@ -51,6 +70,7 @@ module.exports = {
     require("nightwind"),
     require("@tailwindcss/line-clamp"),
     require("nightwind"),
+    require("tailwindcss-radix")(),
     plugin(function ({ addUtilities }) {
       addUtilities({
         ".h-navbar": {
