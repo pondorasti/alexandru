20ec18820cfe6ead8a99bcc6a96169cda291cfee
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-12-26 16:56:44 -0800

    add: LinkPreview component

---
+++ components/LinkPreview/LinkPreview.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image"
+import * as Tooltip from "@radix-ui/react-tooltip"
+import { useTheme } from "next-themes"
+import classNames from "@utils/classNames"
+
+interface ILinkPreview {
+  name: string
+  href: string
+  preview?: string
+  alt: string
+}
+
+export default function LinkPreview({ name, href, alt, preview }: ILinkPreview) {
+  const { resolvedTheme } = useTheme()
+
+  const shimmer = (w: number, h: number, theme?: string) => `
+    <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <rect id="r" width="${w}" height="${h}" fill="${theme === "dark" ? "#0f172a" : "#e2e8f0"}" />
+      <animate attributeName="opacity" values="0.5;1;0.5" dur="2s" repeatCount="indefinite"/>
+    </svg>`
+
+  const toBase64 = (str: string) =>
+    typeof window === "undefined" ? Buffer.from(str).toString("base64") : window.btoa(str)
+
+  return (
+    <Tooltip.Root delayDuration={0}>
+      <Tooltip.Trigger asChild>
+        <a
+          className={classNames(
+            "text-blue-600 hover:text-blue-700",
+            "relative after:absolute after:bottom-[-2px] after:left-0 after:h-[2px] after:w-0 hover:after:w-full after:bg-blue-700 after:rounded-full after:!transition-[width] after:!ease-out after:!duration-200"
+          )}
+          href={href}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {name}
+        </a>
+      </Tooltip.Trigger>
+      <Tooltip.Content
+        side="top"
+        sideOffset={16}
+        className="border border-gray-200 radix-state-close:animate-slide-down radix-state-open:border-5 radix-side-top:animate-slide-up bg-white dark:bg-gray-800 p-2 h-40 w-64 rounded-lg border-gray-200"
+      >
+        <Image
+          src={
+            preview ||
+            `https://api.microlink.io?url=${href}&screenshot=true&meta=false&colorScheme=${
+              resolvedTheme === "dark" ? "dark" : "light"
+            }&embed=screenshot.url`
+          }
+          alt={alt}
+          className="rounded-md overflow-hidden p-2"
+          width={240}
+          height={144}
+          placeholder="blur"
+          blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(240, 144, resolvedTheme))}`}
+          objectFit="cover"
+        />
+      </Tooltip.Content>
+    </Tooltip.Root>
+  )
+}
--- data/secrets.ts
+++ data/secrets.ts
@@ -3,8 +3,8 @@ interface ISecret {
   description: string
   month: string
   year?: number
-  link: string
-  linkPreview?: string
+  href: string
+  preview?: string
 }
 
 const secrets: ISecret[] = [
@@ -13,33 +13,33 @@ const secrets: ISecret[] = [
     description: "Experimental microphone audio visualizer",
     month: "November",
     year: 2021,
-    link: "https://microphone.wtf/",
-    linkPreview: "/images/secrets/microphone.webp",
+    href: "https://microphone.wtf/",
+    preview: "/images/secrets/microphone.webp",
   },
   // {
   //   name: "fishcake.io",
   //   description: "Digital hawker food",
   //   month: "November",
-  //   link: "https://www.fishcake.io/",
+  //   href: "https://www.fishcake.io/",
   // },
   {
     name: "makeschool.fail",
     description: "Conservatory of Make School before it shutdown",
     month: "July",
-    link: "https://makeschool.fail/",
+    href: "https://makeschool.fail/",
   },
   {
     name: "scriptcommands.com",
     description: "Unofficial Marketplace for Raycast Script Commands",
     month: "April",
-    link: "https://scriptcommands.com/",
+    href: "https://scriptcommands.com/",
   },
   {
     name: "boringstocks.live",
     description: "Stocks for the average trader",
     month: "March",
-    link: "https://boringstocks.live",
-    linkPreview:
+    href: "https://boringstocks.live",
+    preview:
       "https://api.microlink.io?url=https%3A%2F%2Fboringstocks.live%2F&screenshot=true&meta=false&waitForTimeout=500&embed=screenshot.url",
   },
   {
@@ -47,79 +47,79 @@ const secrets: ISecret[] = [
     description: "Design Assets of my first startup",
     month: "December",
     year: 2020,
-    link: "https://alexandru.so/secrets/teamo-design",
-    linkPreview: "/images/secrets/teamo-design.webp",
+    href: "https://alexandru.so/secrets/teamo-design",
+    preview: "/images/secrets/teamo-design.webp",
   },
   // {
   //   name: "sushi",
   //   description: "Just a Netflix meme...",
   //   month: "November",
-  //   link: "https://alexandru.so/secrets/sushi",
-  //   linkPreview: "/images/secrets/sushi.webp",
+  //   href: "https://alexandru.so/secrets/sushi",
+  //   preview: "/images/secrets/sushi.webp",
   // },
   // {
   //   name: "amie",
   //   description: "An amie.so clone to hone my skills",
   //   month: "October",
-  //   link: "https://alexandru.so/secrets/amie",
+  //   href: "https://alexandru.so/secrets/amie",
   // },
   {
     name: "the-rise-of-micro-computers",
     description: "Fun design challenge!",
     month: "September",
-    link: "https://alexandru.so/secrets/the-rise-of-micro-computers",
-    linkPreview: "/images/secrets/the-rise-of-micro-computers.webp",
+    href: "https://alexandru.so/secrets/the-rise-of-micro-computers",
+    preview: "/images/secrets/the-rise-of-micro-computers.webp",
   },
   {
     name: "90s-portfolio",
     description: "Portfolio inspired by Raspberry Pi's CLI",
     month: "September",
-    link: "https://alexandru.so/secrets/90s-portfolio",
-    linkPreview: "/images/secrets/90s-portfolio.webp",
+    href: "https://alexandru.so/secrets/90s-portfolio",
+    preview: "/images/secrets/90s-portfolio.webp",
   },
   {
     name: "SimonDraws!",
     description: "Swift Student Challenge recipient of WWDC2020",
     month: "June",
-    link: "https://github.com/Pondorasti/SimonDraws",
-    linkPreview: "/images/secrets/SimonDraws.webp",
+    href: "https://github.com/Pondorasti/SimonDraws",
+    preview: "/images/secrets/SimonDraws.webp",
   },
   {
     name: "StonkScorer",
     description: "FTC Scorer - Skystone (iOS)",
     month: "February",
-    link: "https://github.com/Pondorasti/StonkScorer",
-    linkPreview: "/images/secrets/StonkScorer.webp",
+    href: "https://github.com/Pondorasti/StonkScorer",
+    preview: "/images/secrets/StonkScorer.webp",
   },
   {
     name: "og-personal-website",
     description: "My first proper personal website",
     month: "September",
     year: 2019,
-    link: "https://alexandru.so/secrets/og-personal-website",
-    linkPreview: "/images/secrets/og-personal-website.png",
+    href: "https://alexandru.so/secrets/og-personal-website",
+    preview: "/images/secrets/og-personal-website.png",
   },
   {
     name: "Envision",
     description: "Habit Tracker (iOS)",
     month: "July",
-    link: "https://www.producthunt.com/posts/envision-2",
+    href: "https://www.producthunt.com/posts/envision-2",
   },
   {
     name: "Memoji",
     description: "Scholarship recipient of WWDC2018",
     month: "May",
     year: 2018,
-    link: "https://github.com/Pondorasti/Memoji",
-    linkPreview: "/images/secrets/memoji.webp",
+    href: "https://github.com/Pondorasti/Memoji",
+    preview: "/images/secrets/memoji.webp",
   },
   {
     name: "national-day",
     description: "The first website I've ever created!",
     month: "September",
     year: 2017,
-    link: "https://alexandru.so/secrets/national-day",
-    linkPreview: "/images/secrets/national-day.png",
+    href: "https://alexandru.so/secrets/national-day",
+    preview: "/images/secrets/national-day.png",
   },
 ]
 
