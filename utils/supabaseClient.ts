c01d75393c5ae9e0385569887c3a5bc59c8f495e
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-12-29 14:10:23 -0800

    update: supabaseClient

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,8 +1,7 @@
-import { createClient } from "@supabase/supabase-js"
+import { createClient, SupabaseClient } from "@supabase/supabase-js"
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? ""
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ""
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY ?? ""
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
+const supabase = createClient(supabaseUrl, supabaseKey)
 export default supabase
