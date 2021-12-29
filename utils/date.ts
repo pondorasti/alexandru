7e1a327f8b0de65211a4520e81815a5127b7ac1e
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-12-29 14:06:02 -0800

    add: date utilities

---
+++ utils/date.ts
@@ -0,0 +1,14 @@
+export function normalizeUtc(date: Date): Date {
+  return new Date(date.getTime() - date.getTimezoneOffset() * 60000)
+}
+
+export function formatDate(date: Date): string {
+  const getOrdinalNum = (n: number) =>
+    n + (n > 0 ? ["th", "st", "nd", "rd"][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10] : "")
+
+  const day = getOrdinalNum(date.getDate())
+  const month = date.toLocaleDateString("en-us", { month: "long" })
+  const year = date.toLocaleDateString("en-us", { year: "numeric" })
+
+  return `${month} ${day} ${year}`
+}
