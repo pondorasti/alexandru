48127cdc7c13da936fa77f58a7a8b8a04b5faeaf
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2022-01-01 14:35:53 -0800

    feat: tailwind utilities

--- pages/secrets.tsx
+++ pages/secrets.tsx
@@ -6,8 +6,6 @@ import secrets from "@data/secrets"
 export default function Secret(): JSX.Element {
   const headerStyling = "uppercase text-left text-xs font-semibold tracking-wider text-gray-500 whitespace-nowrap"
   const rowStyling = "p-3 text-gray-900 whitespace-nowrap"
-  const linkStyling = "text-blue-600 hover:text-blue-700"
-  const dividerStyling = "h-full w-px bg-gray-200 bg-opacity-50"
 
   return (
     <main>
@@ -26,9 +24,9 @@ export default function Secret(): JSX.Element {
               <Fragment key={website.name}>
                 <tr className="absolute">
                   <td className="absolute p-0 pr-3 h-12 w-11 -left-11 flex flex-col justify-center items-center">
-                    <div className={classNames(dividerStyling, index === 0 ? "opacity-0" : "")} />
+                    <div className={classNames("divider-y", index === 0 ? "opacity-0" : "")} />
                     {website.year && <div className={classNames(headerStyling, "py-1")}>{website.year}</div>}
-                    <div className={classNames(dividerStyling, index + 1 === secrets.length ? "opacity-0" : "")} />
+                    <div className={classNames("divider-y", index + 1 === secrets.length ? "opacity-0" : "")} />
                   </td>
                 </tr>
                 <tr className={index % 2 === 0 ? "bg-white dark:bg-gray-800" : "bg-gray-50"}>
