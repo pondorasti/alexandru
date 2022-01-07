bfe4cdb5d0996d2fd662de8cdf5135931e8d2144
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2022-01-07 11:10:52 -0800

    feat: Journal staticPaths

---
+++ components/Journal/JournalLayout.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head"
+
+export default function Layout({ children }: { children: JSX.Element }) {
+  return (
+    <article className="prose dark: prose-invert">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/github-dark.min.css"
+        />
+      </Head>
+      {children}
+    </article>
+  )
+}
