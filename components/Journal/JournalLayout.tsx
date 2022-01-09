2c660bbe4da86119e942ab6fe0b64ebf7530918a
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2022-01-09 15:55:44 -0800

    style: prose

--- components/Journal/JournalLayout.tsx
+++ components/Journal/JournalLayout.tsx
@@ -2,13 +2,17 @@ import Head from "next/head"
 
 export default function Layout({ children }: { children: JSX.Element }) {
   return (
-    <article className="prose prose-neutral dark:prose-invert">
+    <article className="prose prose-neutral dark:prose-invert prose-img:rounded-xl prose-pre:rounded-lg prose-a:no-underline prose-headings:font-serif">
       <Head>
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/github-dark.min.css"
         />
       </Head>
+
+      {/* Book Binding */}
+      <div className="fixed border-l-2 border-divider border-dotted h-full -ml-6 top-0" />
+
       {children}
     </article>
   )
