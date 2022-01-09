8dd1ae881d148a05cea1aad0c0c7575e6ea9747e
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2022-01-08 18:37:10 -0800

    style: mobile Description

--- components/Description/Description.tsx
+++ components/Description/Description.tsx
@@ -6,8 +6,8 @@ interface IDescription {
 export default function Description({ title, description }: IDescription): JSX.Element {
   return (
     <>
-      <h1 className="text-2xl sm:text-3xl font-semibold text-gray-900 tracking-tight text-center mb-3">{title}</h1>
-      <p className="text-sm sm:text-base text-gray-500 text-center mb-10">{description}</p>
+      <h1 className="text-3xl font-semibold text-gray-900 tracking-tight text-center mb-3">{title}</h1>
+      <p className="text-base text-gray-500 text-center mb-10">{description}</p>
       <hr />
     </>
   )
