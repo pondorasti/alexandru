acb8e4bc076c4f5cf641add4a09ef12341b23135
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2022-01-09 13:50:11 -0800

    feat(journal): book binding

--- pages/secrets.tsx
+++ pages/secrets.tsx
@@ -11,10 +11,10 @@ export default function Secret(): JSX.Element {
   return (
     <>
       <Description title="Secrets" description="A collection of projects which I have worked on." />
-      <div className="shadow-lg overflow-x-auto overflow-y-hidden border-divider rounded-lg ml-12 md:ml-0">
+      <div className="shadow-lg overflow-x-auto overflow-y-hidden border border-divider rounded-lg ml-12 md:ml-0">
         <table className="table-auto w-full">
           <thead className="bg-gray-50 bg-opacity-50">
-            <tr className="border-b-divider">
+            <tr className="border-b border-divider">
               <th className={classNames(headerStyling, "p-3")}>Website</th>
               <th className={classNames(headerStyling, "p-3")}>Description</th>
             </tr>
@@ -24,9 +24,9 @@ export default function Secret(): JSX.Element {
               <Fragment key={website.name}>
                 <tr className="absolute">
                   <td className="absolute p-0 pr-3 h-12 w-11 -left-11 flex flex-col justify-center items-center">
-                    <div className={classNames("divider-y", index === 0 ? "opacity-0" : "")} />
-                    {website.year && <div className={classNames(headerStyling, "py-1")}>{website.year}</div>}
-                    <div className={classNames("divider-y", index + 1 === secrets.length ? "opacity-0" : "")} />
+                    <aside className={classNames("divider-y", index === 0 ? "opacity-0" : "")} />
+                    {website.year && <aside className={classNames(headerStyling, "py-1")}>{website.year}</aside>}
+                    <aside className={classNames("divider-y", index + 1 === secrets.length ? "opacity-0" : "")} />
                   </td>
                 </tr>
                 <tr className={classNames(index % 2 === 0 ? "bg-gray-200" : "bg-gray-50", "bg-opacity-50")}>
