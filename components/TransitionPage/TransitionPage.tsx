6ba03cacfc2fffcb07660c895bbcc959fc49b99c
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2022-01-18 23:07:44 -0800

    fix: TransitionPage re-renders

--- components/TransitionPage/TransitionPage.tsx
+++ components/TransitionPage/TransitionPage.tsx
@@ -1,31 +1,41 @@
 import { useState, useEffect } from "react"
 import { Transition } from "@headlessui/react"
+import { useRouter } from "next/router"
 
 interface ITransitionPage {
-  children: JSX.Element
+  children: JSX.Element[]
 }
 
 export default function TransitionPage({ children }: ITransitionPage): JSX.Element {
   const [displayChildren, setDisplayChildren] = useState(children)
+  const [displayRoute, setDisplayRoute] = useState("")
   const [show, setShow] = useState(true)
+  const [animateEnter, setAnimateEnter] = useState(true)
+  const router = useRouter()
+
+  const transition = "transition duration-300"
 
   useEffect(() => {
-    if (children !== displayChildren) {
+    if (router.route !== displayRoute) {
       setShow(false)
+      setAnimateEnter(router.route !== "/")
     }
   }, [children])
+
+  console.log(animateEnter)
 
   return (
     <Transition
       show={show}
-      enter="transition duration-300"
+      enter={animateEnter ? transition : ""}
       enterFrom="opacity-0"
       enterTo="opacity-100"
-      leave="transition duration-300"
+      leave={transition}
       leaveFrom="opacity-100"
       leaveTo="opacity-0"
       afterLeave={() => {
         setDisplayChildren(children)
+        setDisplayRoute(router.route)
         setShow(true)
       }}
     >
