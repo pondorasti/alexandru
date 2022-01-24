0ce14f42150d8758d955eb0a224746c151eb9cd1
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2022-01-23 21:39:28 -0800

    style: journal

--- components/JournalLayout/JournalLayout.tsx
+++ components/JournalLayout/JournalLayout.tsx
@@ -2,7 +2,7 @@ import Head from "next/head"
 
 export default function Layout({ children }: { children: JSX.Element }) {
   return (
-    <article className="prose prose-neutral dark:prose-invert prose-img:rounded-xl prose-pre:rounded-lg prose-a:no-underline prose-headings:font-medium prose-headings:font-['IBM_Plex_Sans']">
+    <article className="prose prose-neutral dark:prose-invert prose-img:rounded-xl prose-pre:rounded-lg prose-a:no-underline prose-headings:font-medium prose-headings:font-['IBM_Plex_Sans'] prose-ul:my-5">
       <Head>
         <link
           rel="stylesheet"
