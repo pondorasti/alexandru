ec50645b0f88ffd74f40225ddb28987e269166c3
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2022-01-31 20:08:07 -0800

    feat: screenshot api v3.2

--- pages/api/screenshot.ts
+++ pages/api/screenshot.ts
@@ -10,25 +10,29 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { url, colorScheme } = req.query as IQuery
 
   if (url) {
-    const browser = await chrome.puppeteer.launch({
-      executablePath: await chrome.executablePath,
-      args: chrome.args,
-      headless: chrome.headless,
-    })
+    const browser = await chrome.puppeteer.launch(
+      process.env.NODE_ENV === "production"
+        ? {
+            args: chrome.args,
+            executablePath: await chrome.executablePath,
+            headless: chrome.headless,
+          }
+        : {}
+    )
 
     const page = await browser.newPage()
 
     const viewportOptions = {
       width: 1280,
       height: 800,
-      deviceScaleFactor: 2,
+      deviceScaleFactor: 1,
     }
 
     await page.setViewport(viewportOptions)
     await page.emulateMediaFeatures([{ name: "prefers-color-scheme", value: colorScheme || "light" }])
     await page.goto(url, { waitUntil: "networkidle2" })
 
-    const imageBuffer = await page.screenshot({ type: "webp" })
+    const imageBuffer = await page.screenshot()
 
     await browser.close()
     res.setHeader("Content-Type", "image/webp")
