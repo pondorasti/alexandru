61e2a021912f0e45b90e57f875c94e6627efa757
Alexandru Turcanu <pondorasti@gmail.com>
2022-07-30 22:15:51 -0700

    lower threshold

--- pages/api/screenshot.ts
+++ pages/api/screenshot.ts
@@ -35,7 +35,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     // Max execution time for serverless function is 10 seconds
     // Therefore, we limit page load for up to 9.5 seconds to avoid timeout
     const elapsedTime = Date.now() - startTime
-    const executionTimeout = new Promise(resolve => setTimeout(resolve, 9000 - elapsedTime))
+    const executionTimeout = new Promise(resolve => setTimeout(resolve, 8000 - elapsedTime))
     console.log({ elapsedTime, remainingTime: 9000 - elapsedTime })
     const waitForPageLoad = page.goto(url, { waitUntil: "networkidle2" })
     await Promise.race([waitForPageLoad, executionTimeout])
