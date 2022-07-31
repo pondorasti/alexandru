4a1c28bef18e313343885d452377c329fa239f99
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2022-07-30 22:24:43 -0700

    fix: screenshot api timeout (#122)
    * fix: screenshot api timeout
    
    * smaller timeout
    
    * dynamic timeout
    
    * smoll fix and comments
    
    * please
    
    * lower threshold
    
    * remove comment

--- pages/api/screenshot.ts
+++ pages/api/screenshot.ts
@@ -8,6 +8,7 @@ interface IQuery {
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { url, colorScheme } = req.query as IQuery
+  const startTime = Date.now()
 
   if (url) {
     const browser = await chrome.puppeteer.launch(
@@ -30,7 +31,13 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     await page.setViewport(viewportOptions)
     await page.emulateMediaFeatures([{ name: "prefers-color-scheme", value: colorScheme || "light" }])
-    await page.goto(url, { waitUntil: "networkidle2" })
+
+    // Max execution time for serverless function is 10 seconds
+    // Therefore, we limit page load for up to 9.5 seconds to avoid timeout
+    const elapsedTime = Date.now() - startTime
+    const executionTimeout = new Promise(resolve => setTimeout(resolve, 8000 - elapsedTime))
+    const waitForPageLoad = page.goto(url, { waitUntil: "networkidle2" })
+    await Promise.race([waitForPageLoad, executionTimeout])
 
     const imageBuffer = await page.screenshot()
 
