937d1e4292cbb8ff434718e600a6fa635673f9b4
Alexandru Turcanu <pondorasti@gmail.com>
2022-07-31 21:24:14 -0700

    try custom logic

--- components/TransitionPage.tsx
+++ components/TransitionPage.tsx
@@ -1,5 +1,6 @@
 import { Transition } from "@headlessui/react"
 import React from "react"
+import { NextSeo } from "next-seo"
 
 interface ITransitionPage {
   children: React.ReactNode
@@ -7,18 +8,35 @@ interface ITransitionPage {
 
 export default function TransitionPage({ children }: ITransitionPage): JSX.Element {
   return (
-    <Transition
-      appear
-      show
-      as="div"
-      enter="transition duration-500 ease-in-out"
-      enterFrom="opacity-0"
-      enterTo="opacity-100"
-      leave="transition duration-500 ease-in-out"
-      leaveFrom="opacity-100"
-      leaveTo="opacity-0"
-    >
-      {children}
-    </Transition>
+    <>
+      <NextSeo
+        title="Simple Usage Example"
+        description="A short description goes here."
+        openGraph={{
+          type: "website",
+          locale: "en_IE",
+          url: "https://alexandru.so/",
+          site_name: "Alexandru Turcanu",
+        }}
+        twitter={{
+          handle: "@handle",
+          site: "@pondorasti",
+          cardType: "summary_large_image",
+        }}
+      />
+      <Transition
+        appear
+        show
+        as="div"
+        enter="transition duration-500 ease-in-out"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition duration-500 ease-in-out"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        {children}
+      </Transition>
+    </>
   )
 }
