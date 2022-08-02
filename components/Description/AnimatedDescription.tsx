91b884ad3e1c1f50b6b1a05928d35b71c3c724d4
Alexandru Turcanu <pondorasti@gmail.com>
2022-08-01 21:50:50 -0700

    feat: try lazy AnimatedDescription approach

--- components/Description/AnimatedDescription.tsx
+++ components/Description/AnimatedDescription.tsx
@@ -3,8 +3,9 @@ import classNames from "@lib/classNames"
 import { useEffect, useState } from "react"
 import { titleStyle, descriptionStyle } from "./styles"
 import type { IDescription } from "./types"
+import Description from "./Description"
 
-export default function Description({ title, description, hideBreak = false }: IDescription): JSX.Element {
+export default function AnimatedDescription({ title, description, hideBreak = false }: IDescription): JSX.Element {
   const transition = "transition-all duration-500"
   const [show, setShow] = useState(false)
   useEffect(() => {
@@ -12,26 +13,31 @@ export default function Description({ title, description, hideBreak = false }: I
   }, [])
 
   return (
-    <Transition show={show}>
-      <Transition.Child
-        enter={classNames(transition)}
-        enterFrom="opacity-0 -translate-y-2"
-        enterTo="opacity-100 translate-y-0"
-      >
-        <h1 className={titleStyle}>{title}</h1>
-      </Transition.Child>
-      <Transition.Child
-        enter={classNames(transition, "delay-[300ms]")}
-        enterFrom="opacity-0 -translate-y-2"
-        enterTo="opacity-100 translate-y-0"
-      >
-        <p className={descriptionStyle}>{description}</p>
-      </Transition.Child>
-      {!hideBreak && (
-        <Transition.Child enter={classNames(transition, "delay-[600ms]")} enterFrom="scale-x-0" enterTo="scale-x-100">
-          <hr />
+    <div className="relative">
+      <Transition show={show} as="div" className="absolute w-full">
+        <Transition.Child
+          enter={classNames(transition)}
+          enterFrom="opacity-0 -translate-y-2"
+          enterTo="opacity-100 translate-y-0"
+        >
+          <h1 className={titleStyle}>{title}</h1>
         </Transition.Child>
-      )}
-    </Transition>
+        <Transition.Child
+          enter={classNames(transition, "delay-[300ms]")}
+          enterFrom="opacity-0 -translate-y-2"
+          enterTo="opacity-100 translate-y-0"
+        >
+          <p className={descriptionStyle}>{description}</p>
+        </Transition.Child>
+        {!hideBreak && (
+          <Transition.Child enter={classNames(transition, "delay-[600ms]")} enterFrom="scale-x-0" enterTo="scale-x-100">
+            <hr />
+          </Transition.Child>
+        )}
+      </Transition>
+      <div className="invisible">
+        <Description title={title} description={description} />
+      </div>
+    </div>
   )
 }
