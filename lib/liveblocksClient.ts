f8bb8d0298c73847ccc06b12ee26d3f90a84fc8a
Alexandru Turcanu <pondorasti@gmail.com>
2022-10-18 21:43:42 -0700

    remove: liveblocks demo

--- lib/liveblocksClient.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@liveblocks/client"
-
-const liveblocksKey = process.env.NEXT_PUBLIC_LIVEBLOCKS_TOKEN ?? ""
-
-const liveblocks = createClient({ publicApiKey: liveblocksKey })
-
-export default liveblocks
