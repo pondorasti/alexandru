d458571231924a2054b3f5cff3ef36e3ccf6bc6f
Alexandru Turcanu <pondorasti@gmail.com>
2022-10-21 14:35:04 -0700

    feat: page view middleware

---
+++ middleware.ts
@@ -0,0 +1,36 @@
+import type { NextRequest, NextFetchEvent } from "next/server"
+import { NextResponse } from "next/server"
+
+const PUBLIC_FILE = /\.(.*)$/
+
+export function middleware(req: NextRequest, event: NextFetchEvent) {
+  const pathname = req.nextUrl.pathname
+
+  // Ignore running this middleware when the request is to a serverless function or a file in public/.
+  const isPageRequest = !PUBLIC_FILE.test(pathname) && !pathname.startsWith("/api")
+
+  const sendAnalytics = async () => {
+    const slug = pathname.slice(pathname.indexOf("/")) || "/"
+
+    const url = req.nextUrl.clone()
+    url.pathname = "/api/view"
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        slug,
+      }),
+    })
+
+    if (res.status !== 200) {
+      console.error("Failed to send analytics", res)
+    }
+  }
+
+  // Don't wait for sendAnalytics() to finish before continuing the response.
+  if (isPageRequest) event.waitUntil(sendAnalytics())
+
+  return NextResponse.next()
+}
---
+++ pages/api/view.ts
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import supabase from "@lib/supabase/private"
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // Skip analytics tracking if on development
+  if (process.env.NODE_ENV === "development") return
+
+  if (req.method === "POST") {
+    await supabase.rpc("increment_views", { page_slug: req.body.slug })
+    return res.status(200).send("Success")
+  } else {
+    return res.status(400).send("Invalid request method")
+  }
+}
