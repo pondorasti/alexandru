f02e019c917f37204a909218db473c3e788ffe15
Alexandru Turcanu <pondorasti@gmail.com>
2022-10-21 14:09:07 -0700

    chore: update supabase

--- lib/supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js"
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? ""
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY ?? ""
-
-const supabase = createClient(supabaseUrl, supabaseKey)
-export default supabase
