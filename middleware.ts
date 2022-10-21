ef7a6653a004eb26fc292e4af92193b81d624ce7
Alexandru Turcanu <pondorasti@gmail.com>
2022-10-21 14:54:27 -0700

    feat: matcher logic

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,20 @@
 import type { NextRequest, NextFetchEvent } from "next/server"
 import { NextResponse, userAgent } from "next/server"
 
-const PUBLIC_FILE = /\.(.*)$/
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - static (static files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|static|favicon.ico).*)",
+  ],
+}
 
 export function middleware(req: NextRequest, event: NextFetchEvent) {
   const pathname = req.nextUrl.pathname
-
-  // Ignore running this middleware when the request is to a serverless function or a file in public/.
-  const isPageRequest = !PUBLIC_FILE.test(pathname) && !pathname.startsWith("/api")
   const { isBot } = userAgent(req)
 
   const sendAnalytics = async () => {
@@ -19,12 +26,8 @@ export function middleware(req: NextRequest, event: NextFetchEvent) {
     try {
       const res = await fetch(url, {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          slug,
-        }),
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ slug }),
       })
 
       if (res.status !== 200) console.error("Failed to send analytics", res)
@@ -34,7 +37,7 @@ export function middleware(req: NextRequest, event: NextFetchEvent) {
   }
 
   // Don't wait for sendAnalytics() to finish before continuing the response.
-  if (isPageRequest && !isBot) event.waitUntil(sendAnalytics())
+  if (!isBot) event.waitUntil(sendAnalytics())
 
   return NextResponse.next()
 }
