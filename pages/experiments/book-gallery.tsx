b66f5300f8f25454aa8e94fd2126ae9e5fd5e39c
Alexandru Turcanu <pondorasti@gmail.com>
2022-10-24 13:20:31 -0700

    feat: paper grain texture

--- pages/experiments/book-gallery.tsx
+++ pages/experiments/book-gallery.tsx
@@ -35,13 +35,22 @@ export default function TimeMachine(): JSX.Element {
   return (
     <TransitionPage title={title} description={description}>
       <Description title={title} description={description} />
+      <svg className="invisible absolute inset-0">
+        <defs>
+          <filter id="paper" x="0%" y="0%" width="100%" height="100%">
+            <feTurbulence type="fractalNoise" baseFrequency="0.9" numOctaves="8" result="noise" />
+            <feDiffuseLighting in="noise" lighting-color="white" surfaceScale="1" result="diffLight">
+              <feDistantLight azimuth="45" elevation="35" />
+            </feDiffuseLighting>
+          </filter>
+        </defs>
+      </svg>
       <div role="list" className="flex flex-row justify-center space-x-4">
         {books.map((book, index) => (
           <button
             role="listitem"
             key={book.title}
             onClick={() => {
-              // setFocusedIndex((focusedIndex + 1) % 2)
               if (index === focusedIndex) {
                 setFocusedIndex(-1)
               } else {
@@ -50,23 +59,20 @@ export default function TimeMachine(): JSX.Element {
             }}
             className={clsx(
               "flex shrink-0 flex-row items-center",
-              focusedIndex !== index && "hover:-translate-y-4",
+              focusedIndex !== index && "hover:-translate-y-4 focus:-translate-y-4",
               focusedIndex === index ? "basis-60" : "basis-12",
               animationStyle
             )}
             style={{ perspective: "1000px", WebkitPerspective: "1000px" }}
           >
-            <div className={clsx("z-50 h-full w-[46px] shrink-0 border border-gray-900 py-4 px-2")}>
+            <div className={clsx("z-50 h-full w-[46px] shrink-0 border border-gray-900 bg-white py-4 px-2")}>
               <h2 className="text-lg font-medium" style={{ writingMode: "vertical-lr" }}>
                 {book.title}
               </h2>
             </div>
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src={book.coverUrl}
-              alt={book.title}
+            <div
               className={clsx(
-                "z-10 h-72 w-[193px] shrink-0 origin-left border border-l-0 border-gray-900 bg-cover",
+                "relative z-10 h-72 shrink-0 origin-left overflow-hidden border border-l-0 border-gray-900 brightness-[0.80] contrast-[2.00]",
                 animationStyle
               )}
               style={{
@@ -79,7 +85,14 @@ export default function TimeMachine(): JSX.Element {
                   focusedIndex === index ? "0deg" : "90deg"
                 }) rotateZ(0deg) skew(0deg, 0deg)`,
               }}
-            />
+            >
+              <span
+                aria-hidden
+                className="pointer-events-none fixed top-0 right-0 z-50 h-full w-full opacity-40 [filter:url(#paper)]"
+              />
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src={book.coverUrl} alt={book.title} className={clsx("h-full w-48 bg-cover", animationStyle)} />
+            </div>
           </button>
         ))}
       </div>
