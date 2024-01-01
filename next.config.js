df52ece1a14911fdcbe4764f26d379da9cee9cca
Elyees Tatua <elyeestatua@gmail.com>
2024-01-01 21:48:17 +0300

    "ops(build)"

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next'.NextConfig)} */
-const nextConfig = {
+const withMDX = require('@next/mdx')();
+
+module.exports = withMDX({
   output: 'export',
-  images: { unoptimized: true}
-}
-
-
-module.exports = nextConfig
+  images: { unoptimized: true },
+  // Your other Next.js configurations go here
+});
